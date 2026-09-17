aa29dcf5b8df46c7de98713e03d0105472d8ba6d
Haoxi Yang <yhxbarking@hotmail.com>
2026-09-18 01:18:28 +0800

    Initial commit

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls;
+
+    public Vector2 movementInput;
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovment.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        // HandleJumpingInput
+        // HandleActionInput
+    }
+
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+
+    }
+}
